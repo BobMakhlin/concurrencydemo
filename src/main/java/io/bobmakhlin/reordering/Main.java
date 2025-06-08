36f6cfc50faf8acc7f920274da53d01956616cdc
BobMakhlin <borismakhlin5@gmail.com>
2025-06-08 12:41:15 +0200

    reordering demo
    # Conflicts:
    #	.gitignore

---
+++ src/main/java/io/bobmakhlin/reordering/Main.java
@@ -0,0 +1,46 @@
+package io.bobmakhlin;
+
+public class Main {
+    static int x = 0;
+    static int y = 0;
+    static int r1 = -1;
+    static int r2 = -1;
+
+    public static void main(String[] args) throws InterruptedException {
+        int iterations = 0;
+
+        while (true) {
+            iterations++;
+
+            x = 0;
+            y = 0;
+            r1 = -1;
+            r2 = -1;
+
+            Thread t1 = new Thread(() -> {
+                x = 1;
+                r1 = y;
+            });
+
+            Thread t2 = new Thread(() -> {
+                y = 1;
+                r2 = x;
+            });
+
+            t1.start();
+            t2.start();
+
+            t1.join();
+            t2.join();
+
+            if (r1 == 0 && r2 == 0) {
+                System.out.printf("Found reordering after %,d iterations: r1 = %d, r2 = %d%n", iterations, r1, r2);
+                break;
+            }
+
+            if (iterations % 1_000_000 == 0) {
+                System.out.printf("Still running... %,d iterations%n", iterations);
+            }
+        }
+    }
+}
